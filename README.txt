11d03512d499066733032b1a958e720af6ec589b
Mateus Reis <mateus_reis@yahoo.com>
2011-09-03 15:20:01 -0300

    first commit

---
+++ README.txt
@@ -0,0 +1,8 @@
+Just a basci jquery/html framework for personal projects. 
+
+Feel free to use it.
+
+[]'s
+
+Mateus
+
---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+/*-------------------------------------------------------------------- 
+ * JQuery Plugin: "validateEmail"
+ * by:
+   Mateus Pereira (mateus@laboratorio72.com)
+   http://www.laboratorio72.com
+ *
+ * Description: Compares the heights of the first-children of a provided element 
+ 								  and sets their min-height to the tallest height. Sets in em units by default if pxToEm() method is available.
+ * Dependencies: jQuery library, jQuery Validation 1.7 (http://bassistance.de/jquery-plugins/jquery-plugin-validation/)
+ * Usage Example:  
+ * Version: 1.0, 02.09.2010
+ * Changelog:
+ * 02.09.2010 initial Version 1.0
+--------------------------------------------------------------------*/
+	
+function xxxx(){
+	alert('xxxx');
+}   
