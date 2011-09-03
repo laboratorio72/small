68619b0cc2b614e9838089c24655be81711b3faf
Mateus Reis <mateus_reis@yahoo.com>
2011-09-03 16:10:51 -0300

    new header

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 /*-------------------------------------------------------------------- 
- * JQuery Plugin: "validateEmail"
+ * JS Scripts
  * by:
    Mateus Pereira (mateus@laboratorio72.com)
    http://www.laboratorio72.com
  *
- * Description: Compares the heights of the first-children of a provided element 
- 								  and sets their min-height to the tallest height. Sets in em units by default if pxToEm() method is available.
- * Dependencies: jQuery library, jQuery Validation 1.7 (http://bassistance.de/jquery-plugins/jquery-plugin-validation/)
+ * Description: 
+ * Dependencies: 
  * Usage Example:  
- * Version: 1.0, 02.09.2010
+ * Version: 
  * Changelog:
  * 02.09.2010 initial Version 1.0
 --------------------------------------------------------------------*/
